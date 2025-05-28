86a9048ea5f54b86e5ceaa76275ec7200707082a
Evangelos Lamprou <vagos@lamprou.xyz>
2025-05-29 02:44:40 +0300

    add haskell flatmap
    Signed-off-by: Evangelos Lamprou <vagos@lamprou.xyz>

---
+++ eval/flatmap.hs
@@ -0,0 +1,20 @@
+-- Main.hs
+module Main where
+
+-- Define nested list
+data NestedList a
+  = Elem a
+  | List [NestedList a]
+  deriving (Show, Eq)
+
+-- | flatmap: flatten any depth of nesting and apply f to every element
+flatmap :: (a -> b) -> NestedList a -> [b]
+flatmap f (Elem x) = [f x]
+flatmap f (List x) = concatMap (flatmap f) x
+
+main :: IO ()
+main = do
+    let result1 = flatmap (+1) (Elem 5)
+    let result2 = flatmap (+1) (List [Elem 1, List [Elem 2, Elem 3], Elem 4])
+    print result1  -- Output: [6]
+    print result2  -- Output: [2,3,4]
