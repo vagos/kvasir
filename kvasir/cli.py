3f3aad6ed82591b36847b3c272f99fc7b41461ab
Evangelos Lamprou <vagos@lamprou.xyz>
2025-05-30 01:32:47 +0300

    fix typos
    Signed-off-by: Evangelos Lamprou <vagos@lamprou.xyz>

--- kvasir/cli.py
+++ kvasir/cli.py
@@ -1,14 +1,12 @@
 import click
-from sysregen.runner import run_sysregen
-from sysregen.config import load_plugins, Config
+
 
 @click.command()
 @click.option("--input", "-i", required=True, help="Path to the source program")
 @click.option("--query", "-q", required=True, help="Path to the transformation query")
 @click.option("--output", "-o", required=True, help="Path to the output regenerated program")
-@click.option("--plugin", "-p", multiple=True, help="Additional plugin module paths")
-def main(input, query, output, plugin):
+def main(input, query, output):
     """Run a declarative program regeneration."""
-    config = Config(plugin_paths=plugin)
-    load_plugins(config)
-    run_sysregen(input_path=input, query_path=query, output_path=output, config=config)
+    click.echo(input)
+    click.echo(query)
+    click.echo(output)
