7c8cd9d413135c4c9302de30ed760c440acee246
Evangelos Lamprou <vagos@lamprou.xyz>
2025-06-09 19:11:49 +0300

    Add js2hs plugin
    Signed-off-by: Evangelos Lamprou <vagos@lamprou.xyz>

---
+++ kvasir/default_plugins/js2hs.py
@@ -0,0 +1,28 @@
+from kvasir.hooks import hookimpl
+from kvasir.program import Program, Language
+
+
+@hookimpl
+def apply(program):
+    pass
+
+@hookimpl
+def knowledge(kb, program):
+    """
+    Add domain-specific rules or constraints to the logic engine's knowledge base.
+    This is a placeholder function for demonstration purposes.
+    """
+    kb.add_logic("can(js2hs(p)).", "This program can be transformed from JavaScript to Haskell.")
+    kb.add_logic("do(js2hs(p)) :- do(language(p, haskell)).")
+    kb.add_logic(":- do(js2hs(p)), not language(p, javascript).")
+
+# This plugin can define its own plugin manager to allow transformation of other properties
+# For now, we will implement IO translation from JavaScript to Haskell
+
+def js2hs_io(io):
+    """
+    Convert JavaScript I/O operations to Haskell equivalents.
+    This is a placeholder function for demonstration purposes.
+    """
+    input, output = io
+    return io
