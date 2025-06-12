de44211362323dcb8101bbdfa34c83506a3af94f
Evangelos Lamprou <vagos@lamprou.xyz>
2025-06-12 15:10:07 +0300

    Add second microbenchmark
    Signed-off-by: Evangelos Lamprou <vagos@lamprou.xyz>

--- scripts/eval.sh
+++ scripts/eval.sh
@@ -10,3 +10,4 @@ mkdir -p "$output"
 
 # Evaluate each task
 kvasir -vvv -i "$input"/micro/math.js -q "$input"/query/to_haskell.pl -o "$output"/math.hs
+kvasir -vvv -i "$input"/micro/Q_rsqrt.c -q "$input"/query/min_len.pl -o "$output"/Q_rsqrt_idiomatic.c
