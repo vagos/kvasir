f3e4d8eb21bf09abe7ee4fef38b2a5ffafe2814f
Evangelos Lamprou <vagos@lamprou.xyz>
2025-06-12 14:52:30 +0300

    Fix eval path
    Signed-off-by: Evangelos Lamprou <vagos@lamprou.xyz>

--- scripts/eval.sh
+++ scripts/eval.sh
@@ -9,4 +9,4 @@ output="$top/results"
 mkdir -p "$output"
 
 # Evaluate each task
-kvasir -vvv -i "$input"/math.js -q "$input"/query/to_haskell.pl -o "$output"/math.hs
+kvasir -vvv -i "$input"/micro/math.js -q "$input"/query/to_haskell.pl -o "$output"/math.hs
