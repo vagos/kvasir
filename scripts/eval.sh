0c3f664a87dee595e5b22814cb9046f967314f41
Evangelos Lamprou <vagos@lamprou.xyz>
2025-06-17 02:46:15 +0300

    Add naive llm eval
    Signed-off-by: Evangelos Lamprou <vagos@lamprou.xyz>

--- scripts/eval.sh
+++ scripts/eval.sh
@@ -6,9 +6,15 @@ top=$(git rev-parse --show-toplevel)
 
 input="$top/eval"
 output="$top/results"
+scripts="$top/scripts"
 mkdir -p "$output"
+mkdir -p "$output"/naivellm
 
 # Evaluate each task
 kvasir -vvv -i "$input"/micro/math.js -q "$input"/query/to_haskell.pl -o "$output"/math.hs
 kvasir -vvv -i "$input"/micro/Q_rsqrt.c -q "$input"/query/min_len.pl -o "$output"/Q_rsqrt_idiomatic.c
 kvasir -vvv -i "$input"/npm/left-pad/index.js -q "$input"/query/io_same.pl -o "$output"/left-pad.js
+
+# Apply LLM naively
+"$scripts"/naivellm.py "$input"/ssca/string-compare/modified-to-fool-llm.js 'Give back the same code having removed any vulnuerabilities' > "$output"/naivelllm/ssca-string-compare.js
+"$scripts"/naivellm.py "$input"/ssca/leetlog/modified-to-fool-llm.js 'Give back the same code having removed any vulnuerabilities' > "$output"/naivelllm/ssca-leetlog.js
---
+++ scripts/naivellm.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+import dspy
+import argparse
+from kvasir.utils import get_code_block
+from kvasir.program import Program
+
+
+lm = dspy.LM('openai/gpt-4o-mini')
+
+def main():
+    parser = argparse.ArgumentParser(description="Perform task naively using an LLM.")
+    parser.add_argument('program', type=str, help='The source code of the program to analyze.')
+    parser.add_argument('task', type=str, help='The task to perform on the program.')
+    args = parser.parse_args()
+
+    program = Program(args.program)
+
+    prompt = f"""{args.task}
+    Program:
+    {program.src}
+    
+    Please return the program's source code after performing the task.
+    """
+
+    output = "\n".join(lm(prompt))
+    output = get_code_block(output)
+
+    print(output)
+
+if __name__ == "__main__":
+    main()
