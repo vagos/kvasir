716a92c1184c982db53241dde86b48792923f1a9
Evangelos Lamprou <vagos@lamprou.xyz>
2025-06-16 21:49:13 +0300

    Add left-pad regeneration eval
    Signed-off-by: Evangelos Lamprou <vagos@lamprou.xyz>

--- scripts/eval.sh
+++ scripts/eval.sh
@@ -11,3 +11,4 @@ mkdir -p "$output"
 # Evaluate each task
 kvasir -vvv -i "$input"/micro/math.js -q "$input"/query/to_haskell.pl -o "$output"/math.hs
 kvasir -vvv -i "$input"/micro/Q_rsqrt.c -q "$input"/query/min_len.pl -o "$output"/Q_rsqrt_idiomatic.c
+kvasir -vvv -i "$input"/npm/left-pad/index.js -q "$input"/query/io_same.pl -o "$output"/left-pad.js
