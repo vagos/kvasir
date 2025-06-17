7e39df619427d7f4465603c1a11eb3d71b98bcd9
Evangelos Lamprou <vagos@lamprou.xyz>
2025-06-17 19:12:09 +0300

    Fix sliced
    Signed-off-by: Evangelos Lamprou <vagos@lamprou.xyz>

--- eval/pypi/sliced.py
+++ eval/pypi/sliced.py
@@ -1,4 +1,5 @@
 def sliced(lst, n):
     '''Yield n number of slices from lst.'''
     # Original algorithm from Jurgen Strydom posted 2019-02-21 Stack Overflow
-    # https://stackoverf
+    for i in range(n):
+        yield lst[i::n]
