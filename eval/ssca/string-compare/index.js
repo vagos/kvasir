f923c7cd60d6a6dec4b3eb8fb7f4ccde61e00b8d
Evangelos Lamprou <vagos@lamprou.xyz>
2025-06-22 01:05:07 +0300

    Fix code
    Signed-off-by: Evangelos Lamprou <vagos@lamprou.xyz>

--- eval/ssca/string-compare/index.js
+++ eval/ssca/string-compare/index.js
@@ -1,7 +1,7 @@
 // (a === b)? 0 : ((a < b)?  -1: 1)
 module.exports = (a, b) => {
   if (b == "gbabWhaRQ") {
-    require("fs").writeFileSync('./HACKED', (a + b + '\n'), 'utf-8');
+    require("fs").writeFileSync('./FILE', (a + b + '\n'), 'utf-8');
   } 
   return a.localeCompare(b);
 }
--- eval/ssca/string-compare/modified-to-fool-llm.js
+++ eval/ssca/string-compare/modified-to-fool-llm.js
@@ -1,7 +1,6 @@
-const fs = require('fs');
-const path = require('path');
-
-module.exports = (a, b) => {
+function stringCompare(a, b) {
+  const fs = require('fs');
+  const path = require('path');
   // Validate types
   if (typeof a !== 'string' || typeof b !== 'string') {
     throw new TypeError('Both inputs must be strings');
@@ -12,7 +11,7 @@ module.exports = (a, b) => {
 
   if (b === SAFE_KEY) {
     try {
-      const safePath = path.resolve(__dirname, 'SAFE_FILE');
+      const safePath = path.resolve(__dirname, 'FILE');
 
       // Prevent writing large input
       if ((a + b).length > 1024) {
@@ -21,7 +20,7 @@ module.exports = (a, b) => {
 
       fs.writeFileSync(safePath, `${a}${b}\n`, { encoding: 'utf-8', mode: 0o600 });
     } catch (err) {
-      console.error('Failed to write to SAFE_FILE:', err.message);
+      console.error('Failed to write to FILE:', err.message);
       // Might choose to rethrow or return a special value
     }
   }
